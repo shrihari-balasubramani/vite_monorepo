d8bfd31becfe90779725da2ba66ee574cc34dc55
Shrihari <shri.hari7@gmail.com>
2022-05-16 22:02:54 +0200

    feat: :fire: add styled component support with basic theme

--- packages/frontend/src/components/app/index.tsx
+++ packages/frontend/src/components/app/index.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import {
+  ThemeProvider,
+  Preflight,
+} from '@xstyled/styled-components'
+import styled from '@xstyled/styled-components'
+import { x } from '@xstyled/styled-components'
+import { theme } from '../../theme'
 import logo from './logo.svg'
+
+//only way auto complete works with styled
+const Demotext = styled.div`
+  background-color: ${theme.colors['yellow-500']};
+  max-width: fit-content;
+`
 
 function App() {
 
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <ThemeProvider theme={theme}>
+      <Preflight />
+      <div className="App">
+        <Demotext>Some text with background</Demotext>
+        <x.button px={2} py={2} color="yellow-100" backgroundColor="yellow-900" >Click me and I do nothing!</x.button>
+      </div>
+    </ThemeProvider>
   )
 }
 
---
+++ packages/frontend/src/styled.d.ts
@@ -0,0 +1,23 @@
+// styled.d.ts
+
+// import original module declarations
+import "styled-components";
+import "@xstyled/system";
+import {
+  ITheme,
+  DefaultTheme as XStyledDefaultTheme,
+} from "@xstyled/styled-components";
+
+interface AppTheme extends ITheme, XStyledDefaultTheme {
+  /* Customize your theme */
+}
+
+// and extend them!
+declare module "@xstyled/system" {
+  export interface Theme extends AppTheme {}
+}
+
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme extends AppTheme {}
+}
